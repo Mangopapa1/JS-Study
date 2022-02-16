c3d96091a8deb65702e2ba735c5bc74953c64ff4
Mangopapa1 <gustp26@gmail.com>
2022-02-16 15:20:12 +0900

    slide 구현

--- carousel/js/script.js
+++ carousel/js/script.js
@@ -1,20 +1,37 @@
-const slides = document.querySelector('ul');
-const slide = document.querySelectorAll('.img');
+const slides = document.querySelector('.slides');
+const slide = document.querySelectorAll('.slides img');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
 
 let counter = 1;
 const size = slide[0].clientWidth;
-nextBtn.addEventListener('click', function(){
-    slide.style.transform = 'translateX(-450px)';
-})
+slides.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
+// Buttons
+nextBtn.addEventListener('click', ()=> {
+  if(counter >= slide.length -1) return;
+  slides.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  slides.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
 
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
+prevBtn.addEventListener('click', ()=> {
+  if(counter <= 0) return;
+  slides.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  slides.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
 
-let counter = 1;
-const size = carouselImages[0].clientWidth;
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+// Jump to First/Last Slide
+slides.addEventListener('transitionend', () => {
+    console.log(slide[counter]);
+    if (slide[counter].id === 'last'){
+      slides.style.transition = 'none'; // 트랜지션 효과 없애기
+      counter = slide.length -2; // couter 초기화
+      slides.style.transform = 'translateX(' + (-size * counter) + 'px)'; // 실제 마지막 이미지로 이동.
+    } else if (slide[counter].id === 'first') {
+      slides.style.transition = 'none';
+      counter = slide.length - counter; // couter 초기화
+      slides.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+  });
